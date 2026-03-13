d08cf3de844d3fe3bf3a52f605e6bae4485fe983
tonydelunanevesdeandrade <tony.lunaandrade@gmail.com>
2026-03-13 20:23:17 -0300

    Create dictionary of modern slang terms
    Added a dictionary of modern slang terms with definitions.

---
+++ dicionario_de_girias_incompleto.py
@@ -0,0 +1,19 @@
+meme_dict = {
+            "CRINGE": "Algo vergonhoso ou constrangedor",
+            "STALKER": "Investigar a vida de alguém online",
+            "FLOP": "Usado para descrever algo que não teve sucesso ou que falhou",
+            "NPC": "meu date só falava de whey protein e dizia 'top'. um npc de academia total."
+            "VIBE": "esse rolê aqui tá vibes boas."
+            "MEME PRONTO": "ele tropeçou, caiu na escada e ainda soltou um ‘tô bem’. meme pronto demais."
+            "EMOJI HUMANO": "ela tem a mesma energia do 😭 em tempo integral. emoji humano real."
+            "POV": "POV: você tentando parecer tranquilo enquanto seu crush posta story com outro."
+            "VIRALIZAR": "postei um vídeo do meu vô dançando calypso e viralizou em 6 horas."
+            "TREND": "essa trend de colocar a cara do namorado no corpo do Ken já deu, hein."
+            }
+
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    
+else:
+    # O que devemos fazer se a palavra não for encontrada?
