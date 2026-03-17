c0c0c92f7879ee569a352512498eb1d8c32744f2
tonydelunanevesdeandrade <tony.lunaandrade@gmail.com>
2026-03-17 14:14:40 -0300

    Refactor meme_dict to include duplicate entries
    como o próprio nome já diz e um dicionário de gírias

--- dicionario_de_girias_incompleto.py
+++ dicionario_de_girias_incompleto.py
@@ -1,19 +1,21 @@
+# Surpreenda seus colegas!
 meme_dict = {
             "CRINGE": "Algo vergonhoso ou constrangedor",
             "STALKER": "Investigar a vida de alguém online",
             "FLOP": "Usado para descrever algo que não teve sucesso ou que falhou",
-            "NPC": "meu date só falava de whey protein e dizia 'top'. um npc de academia total."
-            "VIBE": "esse rolê aqui tá vibes boas."
-            "MEME PRONTO": "ele tropeçou, caiu na escada e ainda soltou um ‘tô bem’. meme pronto demais."
-            "EMOJI HUMANO": "ela tem a mesma energia do 😭 em tempo integral. emoji humano real."
-            "POV": "POV: você tentando parecer tranquilo enquanto seu crush posta story com outro."
-            "VIRALIZAR": "postei um vídeo do meu vô dançando calypso e viralizou em 6 horas."
-            "TREND": "essa trend de colocar a cara do namorado no corpo do Ken já deu, hein."
+            "NPC": "meu date só falava de whey protein e dizia 'top'. um npc de academia total.",
+            "VIBE": "esse rolê aqui tá vibes boas.",
+            "MEME PRONTO": "ele tropeçou, caiu na escada e ainda soltou um ‘tô bem’. meme pronto demais.",
+            "EMOJI HUMANO": "ela tem a mesma energia do 😭 em tempo integral. emoji humano real.",
+            "POV": "POV: você tentando parecer tranquilo enquanto seu crush posta story com outro.",
+            "VIRALIZAR": "postei um vídeo do meu vô dançando calypso e viralizou em 6 horas.",
+            "TREND": "essa trend de colocar a cara do namorado no corpo do Ken já deu, hein.",
             }
 
 word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
 
 if word in meme_dict.keys():
-    
+    meme = meme_dict[word]
+    print(meme)
 else:
-    # O que devemos fazer se a palavra não for encontrada?
+    print("item não encontrado")
